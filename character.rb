a6547ef76656347553d1368cb2cd66387087f477
micky12127 <micky12127@gmail.com>
2013-06-02 00:10:08 +0900

    out put random character meigen

---
+++ character.rb
@@ -0,0 +1,25 @@
+#coding: utf-8
+require 'sinatra'
+require 'haml'
+require 'sinatra/reloader'
+
+set :bind, '0.0.0.0'
+
+get '/' do
+
+    meigen = []
+    lines = 0
+
+    open("meigen.txt") { |file|
+        while line = file.gets
+            meigen << line
+            lines += 1
+        end
+    }
+
+    print meigen[rand(lines - 1)]
+
+    @meigen = meigen[rand(lines - 1)]
+    haml :index
+
+end
